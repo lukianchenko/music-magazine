ac4f3144c621841be881f8e4ada85c8f67593578
Serhii Lukianchenko <sergeyluk92@gmail.com>
2022-10-14 18:56:09 +0300

    WIP: Change index page

--- src/magazine/views.py
+++ src/magazine/views.py
@@ -1,26 +1,38 @@
 from unicodedata import category
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, DetailView, ListView
 
-from magazine.models import Article
+from magazine.models import Article, Category
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        
+
         context = super().get_context_data(**kwargs)
-        context['articles_list'] = Article.objects.filter(category__id=2)
+        context['articles'] = Article.objects.filter(category__visible=True)
         return context
 
 
 class AboutView(TemplateView):
     template_name = "about.html"
 
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.filter(category__visible=True)
+        return context
+
 
 class ContactView(TemplateView):
     template_name = "contact.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.filter(category__visible=True)
+        return context
 
 
 class ArticlesListView(ListView):
