160b6040845818a613d45d56f9204cf04c4ea32d
Serhii Lukianchenko <sergeyluk92@gmail.com>
2022-10-26 11:37:00 +0300

    fix: work with views and index page

--- src/magazine/views.py
+++ src/magazine/views.py
@@ -1,4 +1,4 @@
-from unicodedata import category
+import uuid
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, DetailView, ListView
 
@@ -9,9 +9,11 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
-        context['articles'] = Article.objects.filter(category__visible=True)
+        categories = Category.objects.filter(visible=True)
+        categories_ids = [x.id for x in categories if x.articles_count()>0] 
+        context['categories'] = Category.objects.filter(id__in=categories_ids)
+        context['articles'] = Article.objects.select_related("category")
         return context
 
 
@@ -19,9 +21,8 @@ class AboutView(TemplateView):
     template_name = "about.html"
 
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
-        context['articles'] = Article.objects.filter(category__visible=True)
+        context['categories'] = Category.objects.filter(visible=True)
         return context
 
 
@@ -29,9 +30,8 @@ class ContactView(TemplateView):
     template_name = "contact.html"
 
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
-        context['articles'] = Article.objects.filter(category__visible=True)
+        context['categories'] = Category.objects.filter(visible=True)
         return context
 
 
@@ -40,12 +40,16 @@ class ArticlesListView(ListView):
     paginate_by = 4
     template_name = "articles_list.html"
     queryset = Article.objects.all()
-    # context_object_name = "all_articles"
 
 
 class ArticleDetailView(DetailView):
+    model = Article
     template_name = "article.html"
 
-    def get_object(self):
-        id = self.kwargs.get("id")
-        return get_object_or_404(Article, id=id)
+    def get_object(self, queryset=None):
+        return get_object_or_404(Article, uuid=self.kwargs.get("uuid"))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.filter(visible=True)
+        return context
